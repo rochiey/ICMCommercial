31b62709f7cdff78dc7a1e5a6af8cd8da143360d
rochiey <pvmediocre@gmail.com>
2018-06-20 21:34:07 +0800

    dblinker db handling modified

--- src/com/DatabaseLinker.java
+++ src/com/DatabaseLinker.java
@@ -9,46 +9,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import com.DB;
 
 
 public class DatabaseLinker {
-    static Connection conn = null;
-    static Statement stmt = null;
-    static ResultSet rs = null;
+    
     private static JTable varTable;
-    public static void createDB()
-    {
-        try {
-            Properties prop=new Properties();
-            prop.setProperty("user","root");
-            prop.setProperty("password","");
-            conn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ICM",prop);
-            stmt= conn.createStatement();
-        } catch (SQLException ex) {
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        }
-    }
-    private static void dbHandlerUpdates(String query)
-    {
-        try{
-        DB.createDB();
-         stmt.executeUpdate(query);
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-            }
-        finally{
-            try {
-               conn.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(DatabaseLinker.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
+    
     public DatabaseLinker(JTable tableVar,String query)
     {
         varTable = tableVar;
@@ -58,15 +25,15 @@ public class DatabaseLinker {
     {
         DB.createDB();
         try {
-            rs = stmt.executeQuery(query); //MODIFY THIS NOT_FINAL
-            tblData.setModel(DbUtils.resultSetToTableModel(rs));
+            DB.rs = DB.stmt.executeQuery(query); //MODIFY THIS NOT_FINAL
+            tblData.setModel(DbUtils.resultSetToTableModel(DB.rs));
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseLinker.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(null, "<html><center><font size=4>Oops. Something went wrong. Please try again later"
                    + "</font></center></html>", "Error Message", 0);
         }finally{
             try {
-                rs.close();
+                DB.rs.close();
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, "<html><center><font size=4>Oops, something went wrong. Please try again."
                    + "</font></center></html>", "Error Message", 0);
